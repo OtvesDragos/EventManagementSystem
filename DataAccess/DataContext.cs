04eb93fb59291ee8d21f7901bea488c3c4d656c8
OtvesDragos <dragos.otves@yahoo.ro>
2025-06-24 13:23:17 +0300

    Add project files.

---
+++ DataAccess/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Email).IsRequired();
+            entity.Property(e => e.PasswordHash).IsRequired();
+        });
+    }
+}
